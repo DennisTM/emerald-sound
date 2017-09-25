31781bf41d92a036ac7c58a7b4065805f9083510
Fagan <FAGAND1@ig.bz>
2017-09-25 01:30:36 -0400

    Setting up express for heroku

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,25 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import {HttpModule} from '@angular/http';
+import {FormsModule} from '@angular/forms';
+
+import { PostsService } from './posts.service';
 
 import { AppComponent } from './app.component';
 import { HomeComponent } from './home/home.component';
 import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
+import { PostsComponent } from './posts/posts.component';
 
 const appRoutes: Routes = [
   { path: 'home', component: HomeComponent },
   { path: '',
     redirectTo: '/home',
     pathMatch: 'full'
+  },
+  {
+    path: 'posts',
+    component: PostsComponent
   },
   { path: '**', component: PageNotFoundComponent }
 ];
@@ -20,16 +29,19 @@ const appRoutes: Routes = [
   declarations: [
     AppComponent,
     HomeComponent,
-    PageNotFoundComponent
+    PageNotFoundComponent,
+    PostsComponent
   ],
   imports: [
     BrowserModule,
+    FormsModule,
+    HttpModule,
     RouterModule.forRoot(
       appRoutes,
       { enableTracing: true } // <-- debugging purposes only
     )
   ],
-  providers: [],
+  providers: [PostsService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
